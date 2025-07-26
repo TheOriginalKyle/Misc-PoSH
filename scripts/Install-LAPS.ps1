0516490b54764f5e2fdab98dcd31be21550f1ff7
Kyle Bohlander <kyle@iforgotmydomain.com>
2025-07-26 00:29:53 -0700

    Added coffee link

--- scripts/Install-LAPS.ps1
+++ scripts/Install-LAPS.ps1
@@ -75,7 +75,8 @@
     Release Notes: Initial Release
 
 .LICENSE
-    Copyright © 2025 Kyle Pradlander - www.spacethoughts.net/about
+    Copyright © 2025 Kyle Pradlander  - https://spacethoughts.net/about
+    Saved your bacon? Buy me a coffee - https://coff.ee/TheOriginalKyle
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
